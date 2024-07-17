79dd0188ac5892771b0761c981d122948ede1579
johnson434 <ghkddlsrb96@gmail.com>
2024-07-17 15:09:51 +0900

    백업 디렉터리 조회 단순화
    find 명령어를 이용해서 모든 depth를 검색이 가능해서 해당 명령어로 변경함. 이전 코드는 1층씩 검색해서 다음 층을 조회하고 이를 DFS 방식으로 조회했다. 동일하게 작동하겠지만 find 명령어가 훨씬 보기 깔끔하니까 그걸로 사용함

--- ftp.sh
+++ ftp.sh
@@ -10,24 +10,23 @@ exec 3>&1 1>"$LOGFILE" 2>&1
 trap "echo 'ERROR: An error occurred during execution, check log $LOGFILE for details.' >&3" ERR
 trap '{ set +x; } 2>/dev/null; echo -n "[$(date -Is)]  "; set -x' DEBUG
 
-declare -a directories=()
-declare -a files=()
+declare -a backup_directories=()
 
 # Set script location
 SCRIPT_FILE_LOCATION=$(realpath "$0")
 SCRIPT_DIRECTORY=$(dirname "$SCRIPT_FILE_LOCATION")
 
-function read_directories {
+function read_backup_directories {
 	while read -r directory
 	do
-		directories+=($directory)
+		backup_directories+=($directory)
 	done < "$SCRIPT_DIRECTORY/SYNC_DIRECTORIES"
 }
 
 function get_file_list() {
-	while ((${#directories[@]} != 0)); do
-		local current_directory=${directories[0]}
-		directories=("${directories[@]:1}")
+	while ((${#backup_directories[@]} != 0)); do
+		local current_directory=${backup_directories[0]}
+		backup_directories=("${directories[@]:1}")
 
 		# Add current_directory files.
 		cd $current_directory
@@ -36,18 +35,19 @@ function get_file_list() {
 			continue
 		fi
 
-		readarray -d '' file_names < <(find . -maxdepth 1 -type f -print0)
-		for file_name in "${file_names[@]}"; do
-			files+=("${current_directory}/${file_name#./}")
+		readarray -d '' files < <(find . -type f -print0) 
+
+		for ((i = 0; i < ${#files[@]}; i++)); do
+			files[i]="${current_directory}${files[i]#.}"
 		done
 
-		# Add next searching directory
-		local dir=$(ls -d */)
-		local next_directories=("${dir%/*}")
+		for file in "${files[@]}"; do 
+			echo $file; 
+		done
 
 		if [ $? -eq 0 ]; then
-			for next_directory in ${next_directories[@]}; do
-				directories+=("${current_directory}/${next_directory%/}")
+			for next_directory in ${next_backup_directories[@]}; do
+				backup_directories+=("${current_directory}/${next_directory%/}")
 			done
 		fi
 	done
@@ -94,30 +94,20 @@ function backup_file_by_ftp() {
 		echo "Cann't find FTP_DIRECTORY value in FTP_SETTING"
 		return -1
 	fi
-
-	declare local -a pure_file_names=()
-	substract_file_name_from_files pure_file_names
-	echo "pure_file_names_size: ${#pure_file_names[@]}"
-
-	declare local -a pure_directory_name_of_files=()
-	substract_directory_from_files pure_directory_name_of_files
-	echo "pure_directory_name_of_files_size: ${#pure_directory_name_of_files[@]}"
-	echo "pure_file_names: ${pure_file_names[@]}"
-	echo "pure_directory_name_of_files: ${pure_directory_name_of_files[@]}"
-
+	
 	ftp -inv "$SERVER" <<EOF
 		user $USER_NAME $PASSWORD
 		binary
 		pwd
 		cd "/$FTP_DIRECTORY"
-		
 
 		$(for (( i=0; i<${#files[@]}; i++ )); do
-			    f=${files[i]}
 			    backup_directory=${pure_directory_name_of_files[i]}
-			    echo "cd /$FTP_DIRECTORY/$backup_directory"
+			    echo "cd /$FTP_DIRECTORY$backup_directory"
 			    echo "pwd"
-			    echo "lcd $(dirname $f)"
+			    echo "lcd $backup_directory"
+
+			    f=${files[i]}
 			    echo "put ${f##*/}"
 			done
 		)
@@ -141,14 +131,16 @@ function substract_file_name_from_files() {
 	done
 }
 
-read_directories
-echo "read_directories_result: ${directories[@]}"
+read_backup_directories
+get_file_list 
 
-get_file_list
-echo "get_file_list_result : ${files[@]}"
+
+declare -a pure_file_names=()
+substract_file_name_from_files pure_file_names
+echo "pure_file_names: ${pure_file_names[@]}"
+
+declare -a pure_directory_name_of_files=()
+substract_directory_from_files pure_directory_name_of_files
+echo "pure_directory_name_of_files; ${pure_directory_name_of_files[@]}"
 
 backup_file_by_ftp
-
-
-
-
